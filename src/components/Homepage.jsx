3a706e774f8016e44c63604b4daeb869de6bb742
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-12-02 15:36:09 +0530

    dont until footer

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ButtonSignin } from './ButtonSignin';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export const Homepage = () => {
   return (
@@ -48,6 +49,56 @@ export const Homepage = () => {
                     <button className='hover:text-slate-800'>Explore out products <span><ArrowForwardIcon/></span></button>
                     <button className='hover:text-slate-800'>Try kite demo <span><ArrowForwardIcon/></span></button>
                 </div>
+            </div>
+        </div>
+
+        <div>
+            <div className='flex justify-between mt-32'>
+                <div className='w-[50%] flex flex-col gap-y-4'>                    
+                    <h1 className='font-bold text-4xl text-slate-700 mb-4 '>Unbeatable pricing</h1>
+                    <p className='text-slate-800 text-lg'>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <p className='text-[#387ed1] hover:text-slate-800'>See pricing <span><ArrowForwardIcon/></span></p>
+                </div>
+                <div className='flex'>
+                    <div className='border w-[50%]'>
+                        <p className='font-medium text-5xl text-slate-700 ml-24 mt-8 mb-8 '>₹0</p>
+                        <p className=' m-8 text-center'>Free equity delivery and direct mutual funds</p>
+                    </div>
+                    <div className='border w-[50%]'>
+                        <p className='font-medium text-5xl text-slate-700 ml-24 mt-8 mb-8'>₹20</p>
+                        <p className=' m-8 text-center'>Intraday and F&O</p>
+                    </div>
+                </div>
+            </div>
+            <div className='flex mt-32'>
+                <img src="./index-education.svg" alt="" />
+                <div className='flex flex-col w-[50%] ml-24 gap-y-4 mt-20'>
+                    <h1 className='font-bold text-2xl text-slate-700 mb-4 '>Free and open market education</h1>
+                    <p className='text-slate-800 '>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                    <p className='text-[#387ed1] hover:text-slate-800'>Varsity <span><ArrowForwardIcon/></span></p>
+                    <p className='text-slate-800 '>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+                    <p className='text-[#387ed1] hover:text-slate-800'>TradingQ&A <span><ArrowForwardIcon/></span></p>
+                </div>
+            </div>
+            <div className='text-center mt-32 flex flex-col gap-y-4'>
+                <h1 className='font-bold text-2xl text-slate-700 mb-4 '>Open a Zerodha Account</h1>
+                <p className='text-slate-800 mb-4'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <ButtonSignin/>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <img src="./logo.svg" alt="" />
+                    <p>© 2010 - 2023, Zerodha Broking Ltd. All rights reserved.</p>
+                    <div>
+                        <TwitterIcon/>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
     </div>
